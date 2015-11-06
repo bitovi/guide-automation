4a7af661d5c5665e0556db8edc6ccc9c77853ee3
Matthew Phillips <matthew@matthewphillips.info>
2015-11-06 17:11:37 -0500

    adding a lot more stuff
    still not finished

--- bin/guide.js
+++ bin/guide.js
@@ -2,14 +2,18 @@
 var argv = require("yargs")
 	.alias("b", "browser")
 	.default("b", "firefox")
+	.boolean("local")
+	.default("local", false)
 	.demand(1)
 	.usage("Usage: $0 path/to/script.js")
 	.argv;
 var path = require("path");
 
-global._guideAutomationOptions = {
-	browser: argv.browser
-};
+var automate = require("../lib/guide-test")
+automate.setDefaults({
+	browser: argv.browser,
+	local: argv.local
+});
 
 var scriptPth = path.resolve(argv._[0]);
 require(scriptPth);
